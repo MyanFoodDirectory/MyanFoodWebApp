32779795bd904d225cf064b6ab9500df722d51d8
Ei Thantar <eithantar3131996@gmail.com>
2017-06-15 23:54:27 -0700

    add code to category content

--- content/category_content.php
+++ content/category_content.php
@@ -18,7 +18,55 @@
 
                     <div class="card col-md-4 col-md-offset-1 card_modal">
                       <div class="card-img text-center">
-                        <img class="card-img-top" src="../img/food1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="../img/myanmartraditionalfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Myanmar Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/thaifood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Thailand Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/chinesefood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Chinese Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/indianfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Indian Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/fastfood.jpg" alt="Card image cap">
                       </div>
                       <div class="card-block">
                         <h4 class="card-title">Fast Food</h4>
@@ -28,10 +76,10 @@
 
                     <div class="card col-md-4 col-md-offset-1 card_modal">
                       <div class="card-img text-center">
-                        <img class="card-img-top" src="../img/food1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="../img/europeanfood.jpg" alt="Card image cap">
                       </div>
                       <div class="card-block">
-                        <h4 class="card-title">Thailand Food</h4>
+                        <h4 class="card-title">European Food</h4>
                         <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
                       </div>
                     </div>
@@ -42,20 +90,20 @@
 
                     <div class="card col-md-4 col-md-offset-1 card_modal">
                       <div class="card-img text-center">
-                        <img class="card-img-top" src="../img/food1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="../img/koreanfood.jpg" alt="Card image cap">
                       </div>
                       <div class="card-block">
-                        <h4 class="card-title">Thailand Food</h4>
+                        <h4 class="card-title">Korean Food</h4>
                         <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
                       </div>
                     </div>
 
                     <div class="card col-md-4 col-md-offset-1 card_modal">
                       <div class="card-img text-center">
-                        <img class="card-img-top" src="../img/food1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="../img/japanesefood.jpg" alt="Card image cap">
                       </div>
                       <div class="card-block">
-                        <h4 class="card-title">Thailand Food</h4>
+                        <h4 class="card-title">Japanese Food</h4>
                         <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
                       </div>
                     </div>
@@ -66,23 +114,86 @@
 
                     <div class="card col-md-4 col-md-offset-1 card_modal">
                       <div class="card-img text-center">
-                        <img class="card-img-top" src="../img/food1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="../img/kachinfood.jpg" alt="Card image cap">
                       </div>
                       <div class="card-block">
-                        <h4 class="card-title">Thailand Food</h4>
+                        <h4 class="card-title">Kachin Food</h4>
                         <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
                       </div>
                     </div>
 
                     <div class="card col-md-4 col-md-offset-1 card_modal">
                       <div class="card-img text-center">
-                        <img class="card-img-top" src="../img/food1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="../img/kayanfood.jpg" alt="Card image cap">
                       </div>
                       <div class="card-block">
-                        <h4 class="card-title">Thailand Food</h4>
+                        <h4 class="card-title">Kayah Food</h4>
                         <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
                       </div>
                     </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/kayinfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Kayin Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/chinfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Chin Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <div class="row">
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/monfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Mon Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/rakhingfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Rakhing Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                  </div>
+                  <div class="row">
+
+                    <div class="card col-md-4 col-md-offset-1 card_modal">
+                      <div class="card-img text-center">
+                        <img class="card-img-top" src="../img/shanfood.jpg" alt="Card image cap">
+                      </div>
+                      <div class="card-block">
+                        <h4 class="card-title">Shan Food</h4>
+                        <a href="../main/category.php" class="btn btn-primary">More about <span class="glyphicon glyphicon-menu-right"></span> </a>
+                      </div>
+                    </div>
+
+                    
 
                   </div>
 
